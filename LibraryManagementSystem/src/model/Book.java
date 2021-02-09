0376c4e244eed0180450b5ee3296430057ce3b3b
aytuncbakir <aytuncbakir.ab@gmail.com>
2021-02-09 21:35:03 +0200

    Version 1.0

---
+++ LibraryManagementSystem/src/model/Book.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Book {
+	
+	public String name;
+	public String authorName;
+	public String isbn;
+	public String type;
+	
+	
+
+}
---
+++ LibraryManagementSystem/src/model/User.java
@@ -0,0 +1,10 @@
+package model;
+
+public class User {
+
+	public String name;
+	public String userName;
+	public String password;
+	
+	
+}
